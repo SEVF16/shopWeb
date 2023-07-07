ffcf4fab91e2a29234e0fdb24531427b44048310
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2023-07-07 19:23:36 -0400

    Se crea proyecto y primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
